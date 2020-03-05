6fc8fdce5fb9013ccf3e8affd7487409db2f644e
Kory Draughn <korydraughn@renci.org>
2020-03-05 16:08:10 -0500

    Implemented command for creating hard-links!
    - TODO: Needs to accept a replica number or resource hierarchy when creating the hard-link.

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+#include <irods/filesystem/filesystem.hpp>
+#include <irods/filesystem/filesystem_error.hpp>
 #include <irods/irods_plugin_context.hpp>
 #include <irods/irods_re_plugin.hpp>
 #include <irods/irods_re_serialization.hpp>
@@ -6,6 +8,7 @@
 #include <irods/irods_at_scope_exit.hpp>
 #include <irods/irods_state_table.h>
 #include <irods/modDataObjMeta.h>
+#include <irods/msParam.h>
 #include <irods/objInfo.h>
 #include <irods/rcMisc.h>
 #include <irods/rodsError.h>
@@ -15,9 +18,16 @@
 #include <irods/irods_query.hpp>
 #include <irods/rsModDataObjMeta.hpp>
 #include <irods/rsDataObjUnlink.hpp>
+#include <irods/rsPhyPathReg.hpp>
 
+#include "boost/any.hpp"
 #include "json.hpp"
 
+#include "boost/uuid/uuid.hpp"
+#include "boost/uuid/uuid_generators.hpp"
+#include "boost/uuid/uuid_io.hpp"
+
+#include <exception>
 #include <stdexcept>
 #include <string>
 #include <string_view>
@@ -162,22 +172,35 @@ namespace
     {
         auto pep_api_data_obj_rename_post(std::list<boost::any>& rule_arguments, irods::callback& effect_handler) -> irods::error
         {
-            try
-            {
+            try {
                 auto* input = util::get_input_object_ptr<dataObjCopyInp_t>(rule_arguments);
                 auto& conn = *util::get_rei(effect_handler).rsComm;
                 const auto physical_path = util::get_physical_path(conn, input->destDataObjInp.objPath);
 
                 for (auto&& sibling : util::get_sibling_data_objects(conn, input->destDataObjInp.objPath)) {
-                    // TODO Should we throw?
-                    //      Should this be an atomic operation?
-                    //      What should happen if one of many hard-links fails to be updated?
-                    const auto ec = util::set_physical_path(conn, sibling, physical_path);
-                    log::rule_engine::trace("rename post - setting physical path of data object [{}] to [{}] :::: ec = {}", sibling.c_str(), physical_path, ec);
+                    // TODO Check error code.
+                    // Should we throw?
+                    // Should this be an atomic operation?
+                    // What should happen if one of many hard-links fail to be updated?
+                    //
+                    // From code review:
+                    // - Introduce general-purpose batch/bulk catalog statements as API plugin.
+                    // - Supports atomic or individual statements.
+                    if (const auto ec = util::set_physical_path(conn, sibling, physical_path); ec < 0) {
+                        log::rule_engine::error("Could not update physical path of [{}] to [{}]. "
+                                                "Use iadmin modrepl to update remaining data objects.",
+                                                input->destDataObjInp.objPath,
+                                                sibling.c_str());
+
+                        addRErrorMsg(&util::get_rei(effect_handler).rsComm->rError, RE_RUNTIME_ERROR, "");
+                    }
+
+                    //log::rule_engine::trace("Setting physical path of data object [{}] to [{}] :::: ec = {}",
+                    //                        sibling.c_str(),
+                    //                        physical_path, ec);
                 }
             }
-            catch (const std::exception& e)
-            {
+            catch (const std::exception& e) {
                 util::log_exception_message(e.what(), effect_handler);
                 return ERROR(RE_RUNTIME_ERROR, e.what());
             }
@@ -185,10 +208,12 @@ namespace
             return CODE(RULE_ENGINE_CONTINUE);
         }
 
-        auto remove_hard_link(std::list<boost::any>& rule_arguments, irods::callback& effect_handler) -> irods::error
+        auto pep_api_data_obj_unlink_pre(std::list<boost::any>& rule_arguments, irods::callback& effect_handler) -> irods::error
         {
-            try
-            {
+            // TODO Use resource id as the avu unit to identify which replicas are participating in
+            // the hard-link group.
+
+            try {
                 auto* input = util::get_input_object_ptr<dataObjInp_t>(rule_arguments);
                 auto& conn = *util::get_rei(effect_handler).rsComm;
 
@@ -204,7 +229,7 @@ namespace
 
                     if (const auto ec = rsDataObjUnlink(&conn, &unreg_input); ec < 0) {
                         log::rule_engine::error("Could not remove hard-link [{}]", input->objPath);
-                        return ERROR(ec, "Hard-Link update error");
+                        return ERROR(ec, "Hard-Link removal error");
                     }
 
                     log::rule_engine::trace("Successfully removed hard-link [{}]. Skipping operation.", input->objPath);
@@ -214,8 +239,7 @@ namespace
 
                 log::rule_engine::trace("Removing data object ...");
             }
-            catch (const std::exception& e)
-            {
+            catch (const std::exception& e) {
                 util::log_exception_message(e.what(), effect_handler);
                 return ERROR(RE_RUNTIME_ERROR, e.what());
             }
@@ -223,10 +247,137 @@ namespace
             return CODE(RULE_ENGINE_CONTINUE);
         }
 
-        // TODO Remove this.
-        auto skip_operation(std::list<boost::any>&, irods::callback&) -> irods::error
+        class pep_api_data_obj_trim final
         {
-            return CODE(RULE_ENGINE_SKIP_OPERATION);
+        public:
+            pep_api_data_obj_trim() = delete;
+
+            static auto pre(std::list<boost::any>& rule_arguments, irods::callback& effect_handler) -> irods::error
+            {
+                // TODO Use resource id as the avu unit to identify which replicas are participating in
+                // the hard-link group.
+
+                try {
+                    auto* input = util::get_input_object_ptr<dataObjInp_t>(rule_arguments);
+                    auto& conn = *util::get_rei(effect_handler).rsComm;
+
+                    // Unregister the data object.
+                    // Hard-links do NOT appear in the trash.
+                    if (const auto uuid = util::get_uuid(conn, input->objPath); uuid && util::get_data_objects(conn, *uuid).size() > 1) {
+                        log::rule_engine::trace("Removing hard-link [{}] ...", input->objPath);
+
+                        dataObjInp_t unreg_input{};
+                        unreg_input.oprType = UNREG_OPR;
+                        rstrcpy(unreg_input.objPath, input->objPath, MAX_NAME_LEN);
+                        addKeyVal(&unreg_input.condInput, FORCE_FLAG_KW, "");
+
+                        if (const auto ec = rsDataObjUnlink(&conn, &unreg_input); ec < 0) {
+                            log::rule_engine::error("Could not remove hard-link [{}]", input->objPath);
+                            return ERROR(ec, "Hard-Link removal error");
+                        }
+
+                        log::rule_engine::trace("Successfully removed hard-link [{}]. Skipping operation.", input->objPath);
+
+                        return CODE(RULE_ENGINE_SKIP_OPERATION);
+                    }
+
+                    log::rule_engine::trace("Removing data object ...");
+                }
+                catch (const std::exception& e) {
+                    util::log_exception_message(e.what(), effect_handler);
+                    return ERROR(RE_RUNTIME_ERROR, e.what());
+                }
+
+                return CODE(RULE_ENGINE_CONTINUE);
+            }
+
+            static auto post(std::list<boost::any>& rule_arguments, irods::callback& effect_handler) -> irods::error
+            {
+                return CODE(RULE_ENGINE_CONTINUE);
+            }
+
+        private:
+        }; // class pep_api_data_obj_trim
+
+        auto make_hard_link(std::list<boost::any>& rule_arguments, irods::callback& effect_handler) -> irods::error
+        {
+            // Inputs:
+            // - Physical path
+            // - Unique logical path
+            // - UUID
+            //
+            // 1. Register the physical path as the logical path.
+            // 2. Generate a catalog-unique UUID.
+            // 3. Attach the UUID to the new logical path.
+
+            try {
+                auto args_iter = std::begin(rule_arguments);
+                const auto logical_path = boost::any_cast<std::string>(*args_iter);
+                const auto link_name = boost::any_cast<std::string>(*++args_iter);
+                auto& conn = *util::get_rei(effect_handler).rsComm;
+
+                const auto physical_path = util::get_physical_path(conn, logical_path);
+
+                dataObjInp_t input{};
+                addKeyVal(&input.condInput, FILE_PATH_KW, physical_path.data());
+                rstrcpy(input.objPath, link_name.data(), MAX_NAME_LEN);
+
+                if (const auto ec = rsPhyPathReg(&conn, &input); ec < 0) {
+                    log::rule_engine::error("Could not make hard-link [ec = {}, physical_path = {}, link_name = {}]", ec, physical_path, link_name);
+                }
+
+                log::rule_engine::trace("Successfully registered data object [logical_path = {}, physical_path = {}]", logical_path.data(), physical_path.data());
+
+                const auto uuid = [&conn, &logical_path] {
+                    // If a UUID has already been assigned to the source logical path, then return that.
+                    if (const auto uuid = util::get_uuid(conn, logical_path); uuid) {
+                        return std::make_tuple(false, *uuid);
+                    }
+
+                    // Generate an unused UUID and return it.
+                    auto uuid = to_string(boost::uuids::random_generator{}());
+                    auto gql = fmt::format("select COUNT(DATA_NAME) where META_DATA_ATTR_NAME = 'irods::hard_link' and META_DATA_ATTR_VALUE = '{}'", uuid);
+
+                    for (auto&& row : irods::query{&conn, gql}) {
+                        log::rule_engine::trace("UUID [{}] already in use. Generating new UUID ...", uuid);
+                        uuid = to_string(boost::uuids::random_generator{}());
+                        gql = fmt::format("select COUNT(DATA_NAME) where META_DATA_ATTR_NAME = 'irods::hard_link' and META_DATA_ATTR_VALUE = '{}'", uuid);
+                    }
+
+                    return std::make_tuple(true, uuid);
+                }();
+
+                // Get the resource id of the source logical path.
+                const auto resc_id = [&conn, p = fs::path{logical_path}] {
+                    const auto gql = fmt::format("select RESC_ID where COLL_NAME = '{}' and DATA_NAME = '{}'",
+                                                 p.parent_path().c_str(),
+                                                 p.object_name().c_str());
+
+                    for (auto&& row : irods::query{&conn, gql}) {
+                        return row[0];
+                    }
+
+                    THROW(SYS_INTERNAL_ERR, "Could not get resource id for source logical path");
+                }();
+
+                try {
+                    fs::server::set_metadata(conn, link_name, {"irods::hard_link", std::get<std::string>(uuid), resc_id});
+
+                    if (const auto& [new_uuid, uuid_value] = uuid; new_uuid) {
+                        fs::server::set_metadata(conn, logical_path, {"irods::hard_link", uuid_value, resc_id});
+                    }
+                }
+                catch (const fs::filesystem_error& e) {
+                    log::rule_engine::error("Could not set hard-link metadata [msg = {}, ec = {}]", e.what(), e.code().value());
+                    return ERROR(e.code().value(), e.what());
+                }
+            }
+            catch (const std::exception& e) {
+                util::log_exception_message(e.what(), effect_handler);
+                return ERROR(RE_RUNTIME_ERROR, e.what());
+            }
+
+            return SUCCESS();
         }
     } // namespace handler
 
@@ -237,19 +388,22 @@ namespace
     // clang-format off
     using handler_type     = std::function<irods::error(std::list<boost::any>&, irods::callback&)>;
     using handler_map_type = std::map<std::string_view, handler_type>;
-    // clang-format on
 
     const handler_map_type pep_handlers{
         {"pep_api_data_obj_rename_post", handler::pep_api_data_obj_rename_post},
-        {"pep_api_data_obj_unlink_pre",  handler::remove_hard_link},
-        {"pep_api_data_obj_trim_pre",    handler::remove_hard_link},
+        {"pep_api_data_obj_unlink_pre",  handler::pep_api_data_obj_unlink_pre},
+        {"pep_api_data_obj_trim_post",   handler::pep_api_data_obj_trim::post},
+        {"pep_api_data_obj_trim_pre",    handler::pep_api_data_obj_trim::pre}
     };
 
-    const handler_map_type commands{
+    // TODO Could expose these as a new .so. The .so would then be loaded by the new "irods" cli.
+    // Then we get things like: irods ln <args>...
+    const handler_map_type hard_link_handlers{
         {"hard_links_count_links", {}},
         {"hard_links_list_data_objects", {}},
-        {"hard_links_make_link", {}}
+        {"hard_links_make_link", handler::make_hard_link}
     };
+    // clang-format on
 
     template <typename ...Args>
     using operation = std::function<irods::error(irods::default_re_ctx&, Args...)>;
@@ -262,13 +416,13 @@ namespace
 
     auto list_rules(irods::default_re_ctx&, std::vector<std::string>& rules) -> irods::error
     {
-        std::transform(std::begin(pep_handlers),
-                       std::end(pep_handlers),
+        std::transform(std::begin(hard_link_handlers),
+                       std::end(hard_link_handlers),
                        std::back_inserter(rules),
                        [](auto v) { return std::string{v.first}; });
 
-        std::transform(std::begin(commands),
-                       std::end(commands),
+        std::transform(std::begin(pep_handlers),
+                       std::end(pep_handlers),
                        std::back_inserter(rules),
                        [](auto v) { return std::string{v.first}; });
 
@@ -288,6 +442,79 @@ namespace
 
         return CODE(RULE_ENGINE_CONTINUE);
     }
+
+    auto exec_rule_text_impl(std::string_view rule_text, irods::callback effect_handler) -> irods::error
+    {
+        log::rule_engine::debug({{"rule_text", std::string{rule_text}}});
+
+        // irule <text>
+        if (rule_text.find("@external rule {") != std::string::npos) {
+            const auto start = rule_text.find_first_of('{') + 1;
+            rule_text = rule_text.substr(start, rule_text.rfind(" }") - start);
+        }
+        // irule -F <script>
+        else if (rule_text.find("@external") != std::string::npos) {
+            const auto start = rule_text.find_first_of('{');
+            rule_text = rule_text.substr(start, rule_text.rfind(" }") - start);
+        }
+
+        log::rule_engine::debug({{"rule_text", std::string{rule_text}}});
+
+        try {
+            const auto json_args = json::parse(rule_text);
+
+            log::rule_engine::debug({{"function", __func__}, {"json_arguments", json_args.dump()}});
+
+            const auto op = json_args.at("operation").get<std::string>();
+
+            if (const auto iter = hard_link_handlers.find(op); iter != std::end(hard_link_handlers)) {
+                std::list<boost::any> args{
+                    json_args.at("logical_path").get<std::string>(),
+                    json_args.at("link_name").get<std::string>()
+                };
+
+                return (iter->second)(args, effect_handler);
+            }
+
+            return ERROR(INVALID_OPERATION, fmt::format("Invalid operation [{}]", op));
+        }
+        catch (const json::parse_error& e) {
+            // clang-format off
+            log::rule_engine::error({{"rule_engine_plugin", "hard_links"},
+                                     {"rule_engine_plugin_function", __func__},
+                                     {"log_message", e.what()}});
+            // clang-format on
+
+            return ERROR(USER_INPUT_FORMAT_ERR, e.what());
+        }
+        catch (const json::type_error& e) {
+            // clang-format off
+            log::rule_engine::error({{"rule_engine_plugin", "hard_links"},
+                                     {"rule_engine_plugin_function", __func__},
+                                     {"log_message", e.what()}});
+            // clang-format on
+
+            return ERROR(SYS_INTERNAL_ERR, e.what());
+        }
+        catch (const std::exception& e) {
+            // clang-format off
+            log::rule_engine::error({{"rule_engine_plugin", "hard_links"},
+                                     {"rule_engine_plugin_function", __func__},
+                                     {"log_message", e.what()}});
+            // clang-format on
+
+            return ERROR(SYS_INTERNAL_ERR, e.what());
+        }
+        catch (...) {
+            // clang-format off
+            log::rule_engine::error({{"rule_engine_plugin", "hard_links"},
+                                     {"rule_engine_plugin_function", __func__},
+                                     {"log_message", "Unknown error"}});
+            // clang-format on
+
+            return ERROR(SYS_UNKNOWN_ERROR, "Unknown error");
+        }
+    }
 } // namespace (anonymous)
 
 //
@@ -300,10 +527,24 @@ extern "C"
 auto plugin_factory(const std::string& _instance_name,
                     const std::string& _context) -> pluggable_rule_engine*
 {
-    // clang-format off
-    const auto no_op         = [](auto&&...) { return SUCCESS(); };
-    const auto not_supported = [](auto&&...) { return CODE(SYS_NOT_SUPPORTED); };
-    // clang-format on
+    const auto no_op = [](auto&&...) { return SUCCESS(); };
+
+    const auto exec_rule_text_wrapper = [](irods::default_re_ctx&,
+                                                 const std::string& rule_text,
+                                                 msParamArray_t*,
+                                                 const std::string&,
+                                                 irods::callback effect_handler)
+    {
+        return exec_rule_text_impl(rule_text, effect_handler);
+    };
+
+    const auto exec_rule_expression_wrapper = [](irods::default_re_ctx&,
+                                                 const std::string& rule_text,
+                                                 msParamArray_t* ms_params,
+                                                 irods::callback effect_handler)
+    {
+        return exec_rule_text_impl(rule_text, effect_handler);
+    };
 
     auto* re = new pluggable_rule_engine{_instance_name, _context};
 
@@ -312,8 +553,8 @@ auto plugin_factory(const std::string& _instance_name,
     re->add_operation("rule_exists", operation<const std::string&, bool&>{rule_exists});
     re->add_operation("list_rules", operation<std::vector<std::string>&>{list_rules});
     re->add_operation("exec_rule", operation<const std::string&, std::list<boost::any>&, irods::callback>{exec_rule});
-    re->add_operation("exec_rule_text", operation<const std::string&, msParamArray_t*, const std::string&, irods::callback>{not_supported});
-    re->add_operation("exec_rule_expression", operation<const std::string&, msParamArray_t*, irods::callback>{not_supported});
+    re->add_operation("exec_rule_text", operation<const std::string&, msParamArray_t*, const std::string&, irods::callback>{exec_rule_text_wrapper});
+    re->add_operation("exec_rule_expression", operation<const std::string&, msParamArray_t*, irods::callback>{exec_rule_expression_wrapper});
 
     return re;
 }
